f8d9eeaaaa059e48fff4c9d63479717f71859b65
jayzpl <jzasada06@gmail.com>
2022-10-02 13:57:01 +0200

    Added sorting by dates and categories ascending and descending

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,13 +1,24 @@
 from django import forms
 from .models import Expense, Category
+from enum import Enum
+
+
+class Sorting(Enum):
+    ASC = 'Ascending'
+    DSC = 'Descending'
 
 
 class ExpenseSearchForm(forms.ModelForm):
-
     date_from = forms.DateField(required=False)
     date_to = forms.DateField(required=False)
+    date_sorting = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                                             choices=((Sorting.ASC, Sorting.ASC.value),
+                                                      (Sorting.DSC, Sorting.DSC.value)), required=False)
     categories = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
                                            choices=Category.objects.all().values_list().order_by('id'), required=False)
+    categories_sorting = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                                                   choices=((Sorting.ASC, Sorting.ASC.value),
+                                                            (Sorting.DSC, Sorting.DSC.value)), required=False)
 
     class Meta:
         model = Expense
--- expenses/views.py
+++ expenses/views.py
@@ -2,9 +2,17 @@ from datetime import datetime
 
 from django.views.generic.list import ListView
 
-from .forms import ExpenseSearchForm
+from .forms import ExpenseSearchForm, Sorting
 from .models import Expense, Category
 from .reports import summary_per_category
+
+
+def sort_query_if_possible(queryset, sort_type: list[str], field: str):
+    if sort_type[0] == str(Sorting.ASC) and len(sort_type) < 2:
+        queryset = queryset.order_by(f"{field}")
+    if sort_type[0] == str(Sorting.DSC) and len(sort_type) < 2:
+        queryset = queryset.order_by(f"-{field}")
+    return queryset
 
 
 class ExpenseListView(ListView):
@@ -19,17 +27,24 @@ class ExpenseListView(ListView):
             name = form.cleaned_data.get('name', '').strip()
             date_from = form.cleaned_data.get('date_from', '')
             date_to = form.cleaned_data.get('date_to', '')
-            categories = form.cleaned_data.get('categories',)
+            categories = form.cleaned_data.get('categories', )
+            date_sorting = form.cleaned_data.get('date_sorting')
+            categories_sorting = form.cleaned_data.get('categories_sorting')
+
             if name:
                 queryset = queryset.filter(name__icontains=name)
-            if date_from:
+            if date_from and date_to is None:
                 queryset = queryset.filter(date__range=[date_from, datetime.today().date()])
             if date_to and date_from:
                 queryset = queryset.filter(date__range=[date_from, date_to])
-            if date_to:
+            if date_to and date_from is None:
                 queryset = queryset.filter(date__lte=date_to)
             if categories:
                 queryset = queryset.filter(category__id__in=categories)
+            if date_sorting:
+                queryset = sort_query_if_possible(queryset, date_sorting, 'date')
+            if categories_sorting:
+                queryset = sort_query_if_possible(queryset, categories_sorting, 'category')
 
         return super().get_context_data(
             form=form,
@@ -41,4 +56,3 @@ class ExpenseListView(ListView):
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
-
